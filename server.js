4c6ac1b7ada4c9cc33c032880288dcf9c337fcf2
ciro8989 <ciro8989@gmail.com>
2016-07-03 11:14:08 -0300

    Heroku

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
-var express = require("express");
+var express = require('express');
 var bodyParser = require("body-parser");
-var cors = require("cors");
 var mongoskin = require("mongoskin");
 var db = mongoskin.db("mongodb://@localhost:27017/sdvDatabase", {safe:true});
+var cors = require("cors");
 var port = 5000;
+
 
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+
 app.set('port', (process.env.PORT || port));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
 app.post('/login', function(req, res) {
     console.log('Checking login of ' + req.body["username"] + "/" + req.body["password"]);
     var username = req.body["username"];
@@ -23,8 +40,3 @@ app.post('/login', function(req, res) {
             res.send({result:logResult});
         });
 });
-app.listen(port, function()
-    {
-        console.log("listen on " + port);    
-    }
-);
